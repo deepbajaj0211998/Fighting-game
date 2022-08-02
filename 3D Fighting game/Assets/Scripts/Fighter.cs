733eac86987a0446849bbdba036c25bd7bbadae7
deepbajaj0211998 <47936906+deepbajaj0211998@users.noreply.github.com>
2022-08-02 13:18:29 -0700

    Update Fighter.cs

--- 3D Fighting game/Assets/Scripts/Fighter.cs
+++ 3D Fighting game/Assets/Scripts/Fighter.cs
@@ -4,10 +4,37 @@ using UnityEngine;
 
 public class Fighter : MonoBehaviour
 {
+
+    public enum PlayerType
+    {
+        Human, AI
+    };
+
+    public static float maxHealth = 100f;
+    public float life = maxHealth;
+    public string fighterName;
+    public Fighter opponent;
+    public PlayerType player;
+    protected Animator animator;
+    private Rigidbody mybody;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        mybody = GetComponent<Rigidbody>();
+        animator = GetComponent<Animator>();
+    }
+
+    public void UpdateHumanInput()
+    {
+        if(Input.GetAxis("Horizontal") > 0.1)
+        {
+            animator.SetBool("Walk", true);
+        }
+        else
+        {
+            animator.SetBool("Walk", false);
+        }
     }
 
     // Update is called once per frame
