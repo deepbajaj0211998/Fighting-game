78e901f447f107216b80c84f813eaf89b7d16f34
deepbajaj0211998 <47936906+deepbajaj0211998@users.noreply.github.com>
2022-09-04 17:14:48 +0530

    Updated Version of the Game 3.5.

---
+++ Fighting Game 3D/Assets/Scripts/FighterState.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum FighterState 
+{
+   Idle, Walk, WalkBack, Jump, Attack, TakeHit, Defend, Win, KnockOut, None,
+}
---
+++ Fighting Game 3D/Assets/Scripts/FighterStateMachine.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FighterStateMachine : MonoBehaviour
+{
+
+    public FighterState behaviourState;
+    public float horizontalForce;
+    public float verticalForce;
+    public Fighter fighter;
+
+
+
+}
